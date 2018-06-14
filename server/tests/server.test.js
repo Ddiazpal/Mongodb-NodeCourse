f2becd884f9900440358142a8be83c2e45c6b94c
ddiazpal <ddiazpal@unbosque.edu.co>
2018-06-14 15:03:06 -0500

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,23 +4,12 @@
 
     const {app} = require('./../server');
     const {Todo} = require('./../models/todo');
+    const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
+    const {User} = require('./../models/user');
 
-    const todos = [{
-      _id: new ObjectID(),
-      text: 'Fisrt test to do'
-    },{
-      _id: new ObjectID(),
-      text: 'Second test to do',
-      completed: true,
-      completedAt: 333
-    }];
-
-    beforeEach((done) => {
-      Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-      }).then(() => done());
-    });
-
+    beforeEach(populateUsers);
+    beforeEach(populateTodos);
+  
     describe('POST /todos', () => {
       it('should create a new todo', (done) => {
         var text = 'Test todo text';
@@ -91,14 +80,14 @@
         var hexId = new ObjectID().toHexString();
         request(app)
         .get(`/todos/${todos[0].hexIid}`)
-        .expect(400)
+        .expect(404)
         .end(done);
       });
 
       it('Should return 404 for non-object ids', (done) => {
         request(app)
         .get('/todos/123abc')
-        .expect(400)
+        .expect(404)
         .end(done);
       });
     });
@@ -129,14 +118,14 @@
         var hexId = new ObjectID().toHexString();
         request(app)
         .delete(`/todos/${todos[0].hexIid}`)
-        .expect(400)
+        .expect(404)
         .end(done);
       });
 
       it('Should return 404 if objecid is invalid', (done) => {
         request(app)
         .delete('/todos/123abc')
-        .expect(400)
+        .expect(404)
         .end(done);
       });
     });
@@ -179,5 +168,79 @@
         })
         .end(done)
 
+      });
+    });
+
+    describe('GET /users/me', ()=> {
+      it('Should return user if auth ', (done) => {
+        request(app)
+        .get('/users/me')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body._id).toBe(users[0]._id.toHexString());
+          expect(res.body.email).toBe(users[0].email);
+        })
+        .end(done);
+      });
+
+      it('Should return 401 if not auth' , (done) => {
+         request(app)
+         .get('/users/me')
+         .expect(401)
+         .expect((res) => {
+           expect(res.body).toEqual({});
+         })
+         .end(done);
+      });
+    });
+
+    describe('POST /users', () => {
+      it('Should create a user', (done) => {
+        var email = 'example@example.com' ;
+        var password = '123abc';
+
+        request(app)
+        .post('/users')
+        .send({email,password})
+        .expect(200)
+        .expect((res) => {
+          expect(res.headers['x-auth']).toExist();
+          expect(res.body._id).toExist();
+          expect(res.body.email).toBe(email);
+        })
+        .end((err) => {
+          if(err){
+            return done(err);
+          }
+
+          User.findOne({email}).then((user)=>{
+            expect(user).toExist();
+            expect(user.password).toNotBe(password);
+            done();
+          });
+        });
+      });
+      
+      it('Should return validation errors if req invalid', (done) => {
+        request(app)
+        .post('/users')
+        .send({
+          email:'and',
+          password: '25'
+        })
+        .expect(400)
+        .end(done);
+      });
+
+      it('Should not create user if email in use', (done) => {
+        request(app)
+        .post('/users')
+        .send({
+          email: users[0].email,
+          password: 'password123!'
+        })
+        .expect(400)
+        .end(done)
       });
     });

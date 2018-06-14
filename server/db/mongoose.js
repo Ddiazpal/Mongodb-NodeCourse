a889bb88b4b35c6f78da89ff2940201143a72ee6
ddiazpal <ddiazpal@unbosque.edu.co>
2018-06-14 08:57:50 -0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp'); //Connect to Database
+mongoose.connect(process.env.MONGODB_URI); //Connect to Database
 
 module.exports= {mongoose};
